52a7e893d33701b47f6422b649dfef040b9d0be3
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2025-11-24 17:21:08 +0200

    Return detailed error for invalid new password recovery

--- src/auth/routers/handlers/new-password.handler.ts
+++ src/auth/routers/handlers/new-password.handler.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import {AuthService} from "../../application/auth.service";
 import {HttpStatus} from "../../../core/types/http-statuses";
+import {createErrorMessages} from "../../../core/middlewares/validation/input-validation-result.middleware";
 
 export const newPasswordHandler = async (req: Request, res: Response) => {
     const {newPassword, recoveryCode} = req.body as {newPassword: string; recoveryCode: string};
@@ -11,5 +12,7 @@ export const newPasswordHandler = async (req: Request, res: Response) => {
         return;
     }
 
-    res.sendStatus(HttpStatus.BadRequest);
+    res
+        .status(HttpStatus.BadRequest)
+        .json(createErrorMessages([{field: "recoveryCode", message: "recovery code is invalid or expired"}]));
 };
